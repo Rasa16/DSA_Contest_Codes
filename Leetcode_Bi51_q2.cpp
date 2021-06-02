7279214700d70ce5aecdbf119eb62a033f92d1e9
Rahul Sahni <55609204+Rasa16@users.noreply.github.com>
2021-06-02 12:08:20 +0530

    Add files via upload

---
+++ Leetcode_Bi51_q2.cpp
@@ -0,0 +1,27 @@
+class SeatManager {
+public:
+    
+    SeatManager(int n) {
+        for(int i = 1; i<=n; i++)
+            pq.push(i);
+    }
+    
+    int reserve() {
+        int t = pq.top();
+        pq.pop();
+        return t;
+    }
+    
+    void unreserve(int seatNumber) {
+        pq.push(seatNumber);
+    }
+    private:
+        priority_queue<int, vector<int>, greater<int>> pq;
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
