18b9b9c07a3bb9e03d2fefd78fa6703aba237314
Rahul Sahni <55609204+Rasa16@users.noreply.github.com>
2021-06-20 16:33:56 +0530

    Add files via upload

---
+++ Leetcode_674.cpp
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int count=0,mx=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+           if(nums[i]<nums[i+1]) 
+           {
+               count++;
+               mx=max(mx,count);
+           }
+            else
+            {
+                count=0;
+            }
+        }
+         return mx+1;   
+    }
+};
