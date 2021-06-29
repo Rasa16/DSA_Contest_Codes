0d479b33fdc36c962f965814b7e0975ad0666948
Rahul Sahni <55609204+Rasa16@users.noreply.github.com>
2021-06-29 08:55:41 +0530

    Create max_len_chain.cpp

---
+++ max_len_chain.cpp
@@ -0,0 +1,24 @@
+//https://practice.geeksforgeeks.org/problems/max-length-chain/1#
+
+/*You are required to complete this method*/
+bool logic(struct val p, struct val q){
+    return p.first < q.first;
+}
+int maxChainLen(struct val p[],int n)
+{
+//Your code here
+     sort(p, p+n, logic);
+     vector<int>v(n,1);
+     for(int i=1; i<n; i++){
+         for(int j=0; j<i; j++){
+             if(p[i].first > p[j].second && v[i] < v[j]+1){
+                 v[i] = v[j]+1;
+             }
+         }
+     }
+     auto it = v.begin();
+     int ans = INT_MIN;
+     for(; it!= v.end(); it++) ans = max(ans, *it);
+     return ans;
+    
+}
