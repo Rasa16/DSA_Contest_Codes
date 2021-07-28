a001f40b46a4e650c897e114d271a86f9c21908e
Rahul Sahni <55609204+Rasa16@users.noreply.github.com>
2021-07-28 13:57:00 +0530

    Create LL_in_BT.cpp

---
+++ LL_in_BT.cpp
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/linked-list-in-binary-tree/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool find(TreeNode* root, ListNode* head){
+        if(head == NULL)
+            return true;
+        if(root == NULL || root->val != head->val)
+            return false;
+        return find(root->left, head->next) || find(root->right, head->next);
+    }
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if(root == NULL)
+            return false;
+        if(find(root, head) == true)
+            return true;
+        return isSubPath(head, root->left) || isSubPath(head, root->right);
+    }
+};
