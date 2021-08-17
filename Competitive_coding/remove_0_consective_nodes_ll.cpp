77b00cf9cf7c0ac32db6added339f8faa95c60f6
Rahul Sahni <55609204+Rasa16@users.noreply.github.com>
2021-08-17 22:43:13 +0530

    Create remove_0_consective_nodes_ll.cpp

---
+++ Competitive_coding/remove_0_consective_nodes_ll.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        unordered_map<int, ListNode*> m;
+        m[0] = dummy;
+        int sum = 0;
+        while(head){
+            sum += head->val;
+            m[sum] = head;
+            head = head->next;  
+        }
+        head = dummy;
+        sum = 0;
+        while(head){
+            sum += head->val;
+            ListNode* temp = m[sum];
+            if(temp != head)
+                head->next = temp->next;
+            head = head->next;
+        }
+        return dummy->next;
+    }
+};
